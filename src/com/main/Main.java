ec1fe7d50bfd968f1ba67c3d91b8f80abdd45230
omhpatil <patilom887@gmail.com>
2024-11-24 17:12:02 +0530

    UC2 updated

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -2,16 +2,19 @@ package com.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to employee wage computation program");
+        System.out.println( " Welcome Employee Wage Computation Problem" );
         final int FULL_TIME = 1;
+        int WAGE_PER_HR = 20;
+        int workingHours = 0;
         double empType = Math.floor(Math.random() * 10) % 2;
         if (empType == FULL_TIME) {
-            System.out.println(empType);
             System.out.println("Employee is Present");
-        } else {
-            System.out.println(empType);
+            workingHours = 8;
+        }
+        else {
             System.out.println("Employee is Absent");
         }
-
+        int wage = workingHours * WAGE_PER_HR;
+        System.out.println("Employee Daily Wage is " + wage);
     }
 }
