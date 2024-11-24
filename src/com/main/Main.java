99f5c322b6d8c0f8ad049c6fbe5ae1e9d0bd31d4
omhpatil <patilom887@gmail.com>
2024-11-24 17:23:32 +0530

    UC4 updated

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,24 +1,26 @@
 package com.main;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main( String[] args )
+    {
         System.out.println( " Welcome Employee Wage Computation Problem" );
         final int FULL_TIME = 2;
-        int PART_TIME = 1;
+        final int PART_TIME = 1;
         int WAGE_PER_HR = 20;
         int workingHours = 0;
-        double empType = Math.floor(Math.random() * 10) % 2;
-        if (empType == FULL_TIME) {
-            System.out.println("Employee is Present");
-            workingHours = 8;
-        }
-        else if (empType == PART_TIME)
+        int empType =(int) Math.floor(Math.random() * 10) % 2;
+        switch (empType)
         {
-            System.out.println("Employee is Present Part Time");
-            workingHours = 4;
-        }
-        else {
-            System.out.println("Employee is Absent");
+            case FULL_TIME:
+                System.out.println("Employee is Present Full time");
+                workingHours = 8;
+                break;
+            case PART_TIME:
+                System.out.println("Employee is Present Part time");
+                workingHours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
         }
         int wage = workingHours * WAGE_PER_HR;
         System.out.println("Employee Daily Wage is " + wage);
